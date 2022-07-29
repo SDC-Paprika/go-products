c296fc1919a9b670fb612f3925023d70b81ec5bd
Addison Hernandez <addison.hernandez@gmail.com>
2022-07-28 20:55:47 -0400

    Add connection to postgres with gorp

--- db/db.go
+++ db/db.go
@@ -1,1 +1,47 @@
 package db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/go-gorp/gorp"
+	_ "github.com/lib/pq" // postgres
+)
+
+type DB struct {
+	*sql.DB
+}
+
+// TODO: gorm? gorp?
+var db *gorp.DbMap
+
+func Init() {
+	dbConfig := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_NAME"))
+
+	var err error
+	db, err = ConnectDB(dbConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func ConnectDB(dataSourceName string) (*gorp.DbMap, error) {
+	db, err := sql.Open("postgres", dataSourceName)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	dbMap := &gorp.DbMap{Db: db, Dialect: gorp.PostgresDialect{}}
+	//dbmap.TraceOn("[gorp]", log.New(os.Stdout, "golang-gin:", log.Lmicroseconds)) //Trace database requests
+	return dbMap, nil
+}
+
+func GetDB() *gorp.DbMap {
+	return db
+}
