7d2da53377c43ec76e8526b00ac35a68df772ab4
Addison Hernandez <addison.hernandez@gmail.com>
2022-07-31 17:54:18 -0400

    Add model and controller for product details

--- controllers/details.go
+++ controllers/details.go
@@ -2,11 +2,29 @@ package controllers
 
 import (
 	"net/http"
+	"strconv"
 
+	"github.com/SDC-Paprika/go-products/models"
 	"github.com/gin-gonic/gin"
 )
 
-func getDetails(c *gin.Context) {
-	productId := c.Param("productId")
-	c.JSON(http.StatusOK, "Product ID: "+productId)
+type DetailsController struct{}
+
+var detailsModel = new(models.DetailsModel)
+
+func (ctrl DetailsController) GetDetails(c *gin.Context) {
+	var productId int
+	var err error
+
+	if productId, err = strconv.Atoi(c.Param("productId")); err != nil {
+		productId = 1
+	}
+
+	results, err := detailsModel.Get(productId)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusNotAcceptable, gin.H{"Message": "Could not get details"})
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, results)
 }
--- models/details.go
+++ models/details.go
@@ -1,1 +1,47 @@
 package models
+
+import (
+	"context"
+
+	"github.com/SDC-Paprika/go-products/db"
+)
+
+type Details struct {
+	ID           int    `db:"id" json:"id"`
+	Name         string `db:"name" json:"name"`
+	Slogan       string `db:"slogan" json:"slogan"`
+	Description  string `db:"description" json:"description"`
+	Category     string `db:"category" json:"category"`
+	DefaultPrice string `db:"default_price" json:"default_price"`
+	Features     Feats  `json:"features"`
+}
+
+type Feats struct {
+	Feature string `db:"feature" json:"feature"`
+	Value   string `db:"value" json:"value"`
+}
+
+type DetailsModel struct{}
+
+func (d DetailsModel) Get(productId int) (details Details, err error) {
+	// query := "SELECT products.*, (SELECT json_agg(f) FROM (SELECT feature, value FROM features WHERE product_id = $1) AS f) AS features FROM products WHERE id = $1"
+	query := `
+		SELECT
+			p.id,
+			p.name,
+			p.slogan,
+			p.description,
+			p.category,
+			p.default_price,
+			(
+				SELECT json_agg(f)
+				FROM (
+					SELECT feature, value FROM features WHERE product_id = $1
+				) AS f
+			) AS features
+		FROM products p
+		WHERE id = $1`
+
+	err = db.GetDB().QueryRow(context.Background(), query, productId).Scan(&details)
+	return
+}
