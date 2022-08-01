cf8e6d8e64964af1a1f12d3aa52d585118bf4945
Addison Hernandez <addison.hernandez@gmail.com>
2022-08-01 13:36:47 -0400

    Add styles model stub

--- models/styles.go
+++ models/styles.go
@@ -1,1 +1,33 @@
 package models
+
+type Styles struct {
+	ID  int       `db:"id" json:"product_id"`
+	Res []Results `json:"results"`
+}
+
+type Results struct {
+	ID            int             `db:"id" json:"styles_id"`
+	Name          string          `db:"name" json:"name"`
+	OriginalPrice string          `db:"original_price" json:"original_price"`
+	SalePrice     string          `db:"sale_price" json:"sale_price"`
+	Default       bool            `db:"default" json:"default?"`
+	Photos        []Images        `json:"photos"`
+	Skus          map[string]SKUs `json:"skus"`
+}
+
+type Images struct {
+	Thumbnail string `db:"thumbnail_url" json:"thumbnail_url"`
+	URL       string `db:"url" json:"url"`
+}
+
+type SKUs struct {
+	Quantity int    `db:"quantity" json:"quantity"`
+	Size     string `db:"size" json:"size"`
+}
+
+type StylesModel struct{}
+
+func (s StylesModel) Get(productId int) (styles Styles, err error) {
+
+	return
+}
