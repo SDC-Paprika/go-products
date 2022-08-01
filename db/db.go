f49c6c727cef44c35437f81c4cc4ce20794c18c9
Addison Hernandez <addison.hernandez@gmail.com>
2022-08-01 13:36:19 -0400

    Add Queries export

--- db/db.go
+++ db/db.go
@@ -41,3 +41,99 @@ func ConnectDB(connString string) (DB, error) {
 func GetDB() DB {
 	return db
 }
+
+var Queries = map[string]string{
+	// type Queries struct {
+	// 	ProductsQuery string
+	// 	DetailsQuery string
+	// 	RelatedQuery string
+	// 	StylesQuery string
+	// }{
+	"productsQuery": `
+		SELECT
+			*
+		FROM
+			products
+		LIMIT $1 OFFSET $2`,
+
+	"detailsQuery": `
+		SELECT
+			p.id,
+			p.name,
+			p.slogan,
+			p.description,
+			p.category,
+			p.default_price,
+			(
+				SELECT
+					json_agg(f)
+				FROM (
+					SELECT
+						feature,
+						value
+					FROM
+						features
+					WHERE
+						product_id = $1
+				) AS f
+			) AS features
+		FROM
+			products p
+		WHERE
+			id = $1`,
+
+	"relatedQuery": `
+		SELECT
+			related_product_id
+		FROM
+			related
+		WHERE
+			current_product_id = $1`,
+
+	"stylesQuery": `SELECT
+		(
+			SELECT
+				json_agg(style_agg)
+			FROM
+				(
+					SELECT
+						styles.id AS style_id,
+						styles.name,
+						styles.original_price,
+						styles.sale_price,
+						styles.default AS "default?",
+						(
+							SELECT
+								json_agg(photo_agg)
+							FROM
+								(
+									SELECT
+										thumbnail_url,
+										url
+									FROM
+										photos
+									WHERE
+										photos.style_id = styles.id
+								) AS photo_agg
+						) AS photos,
+						(
+							SELECT
+								json_object_agg(
+									skus.id,
+									json_build_object(
+										'quantity', skus.quantity,
+										'size', skus.size
+									)
+								) AS skus
+							FROM
+								skus
+							WHERE
+								skus.style_id = styles.id
+						) AS skus
+					FROM
+						styles
+					WHERE
+						product_id = $1
+				) AS style_agg
+		) AS results`,
+}
